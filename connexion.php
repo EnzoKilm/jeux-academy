3782a36cf56453200712271579bf02ffa487113f
EnzoKilm <enzo-beauchamp@live.fr>
2019-11-13 23:24:19 +0100

    Ajout module de connexion

--- connexion.php
+++ connexion.php
@@ -1,72 +1,45 @@
-<!DOCTYPE html>
-<html lang="fr">
-  <head>
-    <meta charset="utf-8" />
-    <link rel="stylesheet" type="text/css" href="css/style.css" />
-    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-    <title>Jeux Academy | Connexion</title>
-    <!-- Lien pour les icones -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-    <!--https://fr.pngtree.com-->
-    <link rel="icon" type="image/png" href="images/favicon.png" />
-  </head>
-
-  <body>
-
-    <aside id="navigateur">
-        <h4>Top jeux</h4><hr/>
-        <h4>Top catégories</h4><hr/>
-        <h4>Top joueurs</h4><hr/>
-    </aside>
-
-    <header>
-      <h1>Jeux Academy</h1>
-    </header>
-
-    <div id="navbar">
-        <ul>
-            <li><a href="index.html">Accueil</a></li>
-        </ul>
-        <ul>
-            <li>
-                <form class="example" action="action_page.php">
-                    <input type="text" placeholder="Recherche..." name="search">
-                    <button type="submit"><i class="fa fa-search"></i></button>
-                </form>
-            </li>
-        </ul>
-        <ul>
-            <li><a href="connexion.php">Connexion</a></li>
-        </ul>
-    </div>
-
-    <div id="connexion">
-      <h1>Connexion</h1>
-      <?php
-      session_start();
-      $titre="Connexion";
-      include("includes/identifiants.php");
-      include("includes/debut.php");
-      include("includes/menu.php");
-      echo '<p><i>Vous êtes ici</i> : <a href="./index.php">Index du forum</a> Connexion';
-      ?>
-    </div>
-
-    <div id="inscription">
-        <h1>Inscription</h1>
-        <?php
-
-        ?>
-    </div>
-
-
-    <footer>
-
-    </footer>
-
-  </body>
-
-  <!--<script type="text/javascript" src="js/file.js"></script>-->
-
-</html>
+<?php
+session_start();
+if(isset($_POST['pseudo']) && isset($_POST['mdp']))
+{
+    // connexion à la base de données
+    $db_username = 'root';
+    $db_password = 'admindb';
+    $db_name     = 'jeux-academy';
+    $db_host     = 'localhost';
+    $db = mysqli_connect($db_host, $db_username, $db_password,$db_name)
+           or die('could not connect to database');
+    
+    // on applique les deux fonctions mysqli_real_escape_string et htmlspecialchars
+    // pour éliminer toute attaque de type injection SQL et XSS
+    $pseudo = mysqli_real_escape_string($db,htmlspecialchars($_POST['pseudo'])); 
+    $mdp = mysqli_real_escape_string($db,htmlspecialchars($_POST['mdp']));
+    
+    if($pseudo !== "" && $mdp !== "")
+    {
+        $requete = "SELECT count(*) FROM users where 
+              pseudo = '".$pseudo."' and mdp = '".$mdp."' ";
+        $exec_requete = mysqli_query($db,$requete);
+        $reponse      = mysqli_fetch_array($exec_requete);
+        $count = $reponse['count(*)'];
+        if($count!=0) // nom d'utilisateur et mot de passe correctes
+        {
+           $_SESSION['pseudo'] = $pseudo;
+           header('Location: principale.php');
+        }
+        else
+        {
+           header('Location: login.php?erreur=1'); // utilisateur ou mot de passe incorrect
+        }
+    }
+    else
+    {
+       header('Location: login.php?erreur=2'); // utilisateur ou mot de passe vide
+    }
+}
+else
+{
+   header('Location: login.php');
+}
+mysqli_close($db); // fermer la connexion
+?>
---
+++ login.php
@@ -0,0 +1,46 @@
+<html>
+    <head>
+       <meta charset="utf-8">
+        <!-- importer le fichier de style -->
+        <link rel="stylesheet" href="css/test_style.css" media="screen" type="text/css" />
+    </head>
+    <body>
+        <div id="container">
+            <!-- zone de connexion -->
+            
+            <form method="post" action="connexion.php">
+                <h1>Connexion</h1>
+                
+                <label><b>Pseudo</b></label>
+                <input type="text" placeholder="Entrer le nom d'utilisateur" name="pseudo" required>
+
+                <label><b>Mot de passe</b></label>
+                <input type="password" placeholder="Entrer le mot de passe" name="mdp" required>
+
+                <input type="submit" id='submit' value='Connexion' >
+                <?php
+                if(isset($_GET['erreur'])){
+                    $err = $_GET['erreur'];
+                    if($err==1 || $err==2)
+                        echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+                }
+                ?>
+            </form>
+
+            <form method="post" action="inscription.php">
+                <h1>Inscription</h1>
+                <label><b>Nom</b></label>
+                <input type="text" placeholder="Entrer votre nom" name="nom" required>
+                <label><b>Prénom</b></label>
+                <input type="text" placeholder="Entrer votre prénom" name="prenom" required>
+                <label><b>Email</b></label>
+                <input type="email" placeholder="Entrer votre email" name="email" required>
+                <label><b>Mot de passe</b></label>
+                <input type="password" placeholder="Entrer un mot de passe" name="mdp" required>
+                <label><b>Confirmation du mot de passe</b></label>
+                <input type="password" placeholder="Entrer à nouveau le mot de passe" name="repmdp" required><br><br>
+                <input type="submit" name="submit" value="Inscription"> 
+            </form>
+        </div>
+    </body>
+</html>
