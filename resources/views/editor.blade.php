2b4d1343e65f46529a9c7b7cb701f3c9218906cf
EnzoKilm <enzo-beauchamp@live.fr>
2020-03-27 19:56:11 +0100

    Modifs editor

--- resources/views/editor.blade.php
+++ resources/views/editor.blade.php
@@ -2,23 +2,34 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-            <div class="editor">
-                <div class="card-header">Création de jeu</div>
+    <div class="row">
+        <div class="editor">
+            <div class="card-header">Création du jeu</div>
 
-                <div class="card-body">
-                    Amuse toi bien !
+            <div class="card-body">
+                <div class="containerEditor" method="post">
+                    <h2>Ecrivez votre code JS</h2>
+                    <form class="editCode" action="{{ route('editor') }}">
+                        <textarea id="codeUser" name="codeUser"></textarea>
+                        <input type="submit" value='run'>
+                    </form>
                 </div>
             </div>
         </div>
-        <div class="col-md-6">
-            <div class="render">
-                <div class="card-header">Création de jeu</div>
+        <div class="render">
+            <div class="card-header">Rendu</div>
 
-                <div class="card-body">
-                    Amuse toi bien !
-                </div>
+            <div class="card-body render-body">
+                <canvas id="cassebriques" width="600" height="500"></canvas>
+                <script>
+                var canvas = document.getElementById("cassebriques");
+                var ctx = canvas.getContext("2d");
+                </script>
+                <?php
+                if (isset($_REQUEST['codeUser'])){
+                echo "<script>".$_REQUEST['codeUser']."</script>";
+                }
+                ?>
             </div>
         </div>
     </div>
