9620f2af2f0db0fcff177e466b991c5ec0756379
EnzoKilm <enzo-beauchamp@live.fr>
2020-03-27 14:14:05 +0100

    Modif redirect page connexion quand nécessaire

--- resources/views/auth/login-error.blade.php
+++ resources/views/auth/login-error.blade.php
@@ -4,10 +4,11 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            <div class="card-top">
+                Erreur, vous devez être connecté pour accéder à cette page.
+            </div>
             <div class="card">
-                <div class="card-header">{{ __('Connexion') }}
-                    Vous devez être connectés pour accéder à cette page.
-                </div>
+                <div class="card-header">{{ __('Connexion') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
