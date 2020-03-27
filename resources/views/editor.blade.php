dedc864973928944a551ad31ef1e8a45cdb15fe0
EnzoKilm <enzo-beauchamp@live.fr>
2020-03-27 19:11:40 +0100

    modif

--- resources/views/editor.blade.php
+++ resources/views/editor.blade.php
@@ -3,8 +3,23 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-6">
             <div class="editor">
+                <div class="card-header">Création de jeu</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    Amuse toi bien !
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="render">
                 <div class="card-header">Création de jeu</div>
 
                 <div class="card-body">
